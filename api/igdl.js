2f2d6733c4beb52fadcbab9e2efcdd6b2a85bbde
ZIKKY STAR <zikkystar0001@gmail.com>
2025-11-26 00:14:40 +0100

    Delete api/igdl.js

--- api/igdl.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-  res.json({ ok: true, test: "Function is working!" });
-}
